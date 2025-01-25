0fa9a8649b030205c3d18cbc3434ead542bf5927
Juan David Lasso <juandavid.lasso@dialapplet.com>
2025-01-25 17:17:44 -0500

    proyecto nuevo

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import * as cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     // Dev
     // const app = await NestFactory.create(AppModule, { cors: { origin: 'http://localhost:3001' } });
     // Prod
-    const app = await NestFactory.create(AppModule, { cors: { origin: 'https://agricola-cliente.vercel.app/' } });
+    const app = await NestFactory.create(AppModule, { cors: { origin: 'https://agricola-cliente.vercel.app' } });
 
-    app.enableCors({ origin: 'https://agricola-cliente.vercel.app/' });
+    // Dev
+    // app.enableCors({ origin: 'http://localhost:3001' });
+    // Prod
+    app.enableCors({ origin: 'https://agricola-cliente.vercel.app' });
 
     app.useGlobalPipes(
         new ValidationPipe({
